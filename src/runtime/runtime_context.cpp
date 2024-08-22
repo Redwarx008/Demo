7afb35e49672d1ba626b997159a746e3016ea066
Redwarx008 <50837890+Redwarx008@users.noreply.github.com>
2024-08-23 01:32:59 +0800

    add window_system

---
+++ src/runtime/runtime_context.cpp
@@ -0,0 +1,14 @@
+#include "runtime_context.h"
+
+namespace GameCore
+{
+	void RuntimeContext::startSystems(const std::string& config_file_path)
+	{
+
+	}
+
+	void RuntimeContext::shutdownSystems()
+	{
+
+	}
+}
---
+++ src/runtime/runtime_context.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <memory>
+#include <string>
+
+namespace GameCore
+{
+    class LogSystem;
+    class InputSystem;
+    //class PhysicsManager;
+    class FileSystem;
+    //class AssetManager;
+    class ConfigManager;
+    class WorldManager;
+    class RenderSystem;
+    class WindowSystem;
+    //class ParticleManager;
+
+    struct RuntimeContext
+    {
+        // create all global systems and initialize these systems
+        void startSystems(const std::string& config_file_path);
+        // destroy all global systems
+        void shutdownSystems();
+
+        std::shared_ptr<LogSystem>         m_logger_system;
+        std::shared_ptr<InputSystem>       m_input_system;
+        std::shared_ptr<FileSystem>        m_file_system;
+        //std::shared_ptr<AssetManager>      m_asset_manager;
+        std::shared_ptr<ConfigManager>     m_config_manager;
+        std::shared_ptr<WorldManager>      m_world_manager;
+        //std::shared_ptr<PhysicsManager>    m_physics_manager;
+        std::shared_ptr<WindowSystem>      m_window_system;
+        std::shared_ptr<RenderSystem>      m_render_system;
+        //std::shared_ptr<ParticleManager>   m_particle_manager;
+        //std::shared_ptr<DebugDrawManager>  m_debugdraw_manager;
+        //std::shared_ptr<RenderDebugConfig> m_render_debug_config;
+    };
+}
